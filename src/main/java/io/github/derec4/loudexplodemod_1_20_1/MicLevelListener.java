f2db568eff6b898a31e459e385452b521e78fb60
Derek Chen <derex1987@gmail.com>
2024-09-02 23:05:24 -0500

    feat: initial 1.20.1 backport

---
+++ src/main/java/io/github/derec4/loudexplodemod_1_20_1/MicLevelListener.java
@@ -0,0 +1,42 @@
+package io.github.derec4.loudexplodemod_1_20_1;
+
+import io.github.derec4.loudexplodemod_1_20_1.gamerule.EnableExplodeGamerule;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
+
+import javax.annotation.Nullable;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class MicLevelListener {
+    private static final Logger LOGGER = LogUtils.getLogger();
+    private static final MicLevelDetector micLevelDetector = new MicLevelDetector();
+
+    @SubscribeEvent
+    public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        if (event.phase == TickEvent.Phase.END) {
+            Player player = event.player;
+            execute(player.level(), player.getX(), player.getY(), player.getZ(), player);
+        }
+    }
+
+    private static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+        if (micLevelDetector.isMicLevelHigh()) {
+            if (entity instanceof Player player) {
+                if (world instanceof Level level && !level.isClientSide && level.getGameRules().getBoolean(EnableExplodeGamerule.EXPLODE_GAMERULE)) {
+                    if (!player.isSpectator() && player.isAlive()) {
+                        level.explode(null, x, y, z, 5, Level.ExplosionInteraction.TNT);
+                        LOGGER.info("EXPLODE");
+                    }
+                }
+            }
+        }
+    }
+}
