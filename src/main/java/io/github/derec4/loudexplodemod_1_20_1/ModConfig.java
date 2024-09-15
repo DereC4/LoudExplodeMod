dd5beb3f55d525897b151fdc752e53e0f0e3730a
Derek Chen <derex1987@gmail.com>
2024-09-15 16:13:24 -0500

    feat: removed cloth config replaced with Resourceful Config

--- src/main/java/io/github/derec4/loudexplodemod_1_20_1/ModConfig.java
+++
@@ -1,9 +0,0 @@
-package io.github.derec4.loudexplodemod_1_20_1;
-
-public class ModConfig {
-    public static double doublesliderValue;
-
-    public static void save() {
-        // Logic to save the config, such as writing to a JSON or TOML file
-    }
-}
